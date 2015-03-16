b4b130f664475cad5f60e33ec212bb26bd5a330e
Rutger Laurman <lekkerduidelijk@gmail.com>
2015-03-16 17:10:49 +0100

    Update main.js
    Merge plugins.js content into main.js
    Add more defaults

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,7 +1,33 @@
-/* Author:
+/* Author: Rutger Laurman - lekkerduidelijk.nl */
 
-*/
+// Avoid `console` errors in browsers that lack a console.
+!function(){for(var a,b=function(){},c=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],d=c.length,e=window.console=window.console||{};d--;)a=c[d],e[a]||(e[a]=b)}();
 
+// Global debug logger
+var log = function(s){
+  if (typeof(js_debug) !== 'undefined' && js_debug) {
+    console.log(s);
+  }
+};
 
+// APP namespace
+var APP = (function($){
 
+  // Object scope settings
+  var s = {};
 
+  // Init method
+  function init() {
+    log("APP.init()")
+
+  }
+
+  // Expose to public
+  return { init: init }
+
+})(jQuery);
+
+// Document ready
+jQuery(document).ready(function(){
+  APP.init();
+});
