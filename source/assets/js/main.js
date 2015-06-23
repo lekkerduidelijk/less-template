44516ddb48c4377ebcf3aae79c13c9b45b2b792a
Rutger Laurman <lekkerduidelijk@gmail.com>
2015-06-23 11:32:21 +0200

    Small refactor

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,4 +1,9 @@
-/* Author: Rutger Laurman - lekkerduidelijk.nl */
+/**
+ * Main app
+ *
+ * @author: Rutger Laurman - lekkerduidelijk.nl
+ * @url: https://github.com/lekkerduidelijk/less-template
+ */
 
 // Avoid `console` errors in browsers that lack a console.
 !function(){for(var a,b=function(){},c=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],d=c.length,e=window.console=window.console||{};d--;)a=c[d],e[a]||(e[a]=b)}();
